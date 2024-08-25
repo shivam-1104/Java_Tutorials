b8a068bf6e362021ed3003862bd68871ac86a63c
Shivam <jaiswalshivam22121@gmail.com>
2024-08-25 19:24:19 +0530

    first commit

---
+++ src/literals.java
@@ -0,0 +1,14 @@
+public class literals {
+    public static void main(String[] args) {
+        int i = 14;
+        float f = 1.1f;
+        double d = 4.5;
+        char ch = 'j';
+        byte age = 23;
+        long ageLo = 6184146294l;
+        short sht = 45;
+        boolean b = true;
+        String name = "James";
+        System.out.println(i);
+    }
+}
---
+++ src/takingInput.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class takingInput {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number :");
+//        int a = sc.nextInt();
+//
+//        System.out.println("Enter a number 1 :");
+//        int a = sc.nextInt(); // we can do float double string and etc
+//        System.out.println("Enter a number 2:");
+//        int b = sc.nextInt();
+//        int sum = a + b ;
+//
+//        System.out.print("The Sum of the number is: ");
+//        System.out.println(sum);
+
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+//        String str = sc.next();
+        String str = sc.nextLine();
+        System.out.println(str);
+
+    }
+}
